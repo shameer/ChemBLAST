d1c343eb8eebf21162d1e130d08d25d6b4da9b00
Syed Asad Rahman <s9asad@gmail.com>
2014-12-30 15:08:21 +0000

    added top hits

--- search.sh
+++ search.sh
@@ -1,5 +1,5 @@
 # Usage
-#sh format.sh data/database.txt "C[C@H](O)C(O)=O"
+#sh format.sh data/database.txt "C[C@H](O)C(O)=O -top 5"
 #
 
-java -jar bin/ChemBlast.jar -searchDB $1 -querySMI $2
+java -jar bin/ChemBlast.jar -searchDB $1 -querySMI $2 -top $3
